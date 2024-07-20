d5596ab8e42903877d09683753e07fc4cf951a6e
Никитин Ермолай Геннадьевич <nikitin.eg@corp.inbres.ru>
2024-07-21 00:00:25 +0300

    сообщения в комнатах

--- ChatRoom/chat/consumers.py
+++ ChatRoom/chat/consumers.py
@@ -1,15 +1,17 @@
 import json
 from django.contrib.auth.models import User
-from .models import Profil, ChatRoom, Message
+from .models import ChatRoom, Message, Profil
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
 
-def userCard(user_id):
-    objects = Profil.objects.filter(user_id=user_id)
-    list = {'UserList': 'UserList'}
+def messagelist(id):
+    objects = Message.objects.filter(chat_room=id)
+    name = ChatRoom.objects.get(id=id).name
+    list = {'MessageList': name}
     for object in objects:
-        list[object.id] = object.name
+        message = {object.author.user.first_name: object.message_text}
+        list[object.id] = message
     return list
 
 
@@ -25,16 +27,59 @@ class WSConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         message = json.loads(text_data)
 
-        if 'load' in message:
-            user_id = message['load']
-            self.send(json.dumps(userCard(user_id)))
-            print('данные пользователя отправлены')
-
-        if 'order' in message:
-            if message['order'] == 'changeFirstName':
+        if 'replacement' in message:
+            if message['replacement'] == 'changeFirstName':
                 id = message['id']
                 name = message['name']
                 user = User.objects.get(id=id)
                 user.first_name = name
                 user.save()
 
+        if 'delete_room' in message:
+            id = message['delete_room']
+            print('комната с ID', id, "удалена")
+            room = ChatRoom.objects.get(id=id)
+            room.delete()
+            print('комната с ID', id, "удалена")
+            # async_to_sync(self.channel_layer.group_send)("all_instructions",
+            #                                              {"type": "all_user", "order": "send_list_rooms"})
+
+        if 'create_room' in message:
+            name = message['create_room']
+            if not ChatRoom.objects.filter(name=name).exists():
+                room = ChatRoom(name=name)
+                room.save()
+                # async_to_sync(self.channel_layer.group_send)("all_instructions",
+                #                                              {"type": "all_user", "order": "send_list_rooms"})
+            else:
+                self.send(json.dumps({'message': 'Выбери другое имя'}))
+                print('совпадение имени комнаты')
+
+        if 'change_room' in message:
+            id = message['change_room']
+            name = message['name']
+            if not ChatRoom.objects.filter(name=name).exists():
+                room = ChatRoom.objects.get(id=id)
+                room.name = name
+                room.save()
+                # async_to_sync(self.channel_layer.group_send)("all_instructions",
+                #                                              {"type": "all_user", "order": "send_list_rooms"})
+            else:
+                self.send(json.dumps({'message': 'Выбери другое имя'}))
+                print('совпадение имени комнаты')
+
+        if 'load' in message:
+            if message['load'] == 'messageList':
+                self.send(json.dumps(messagelist(message['room_id'])))
+
+        if 'usersendcommandroom' in message:
+            if message['usersendcommandroom'] == 'message':
+                room_id = str(message['room_id'])
+                user_id = message['userid']
+                message = message['message']
+                print(room_id, user_id, message)
+                message_save = Message(author=Profil.objects.get(user=user_id), chat_room=ChatRoom.objects.get(id=room_id), message_text=message)
+                message_save.save()
+                print('Сообщение', message, 'сохранено в базе')
+                # async_to_sync(self.channel_layer.group_send)(room_id, {"type": "incoming_message", "order": "accept_message", "name": username, "message": message})
+                print('Сообщение', message, 'отправлено в комнату', room_id)
--- ChatRoom/static/js/jsChat.js
+++ ChatRoom/static/js/jsChat.js
@@ -1,6 +1,27 @@
 const divProfil = document.querySelector('.div_profil');
+const divRooms = document.querySelector('.div_rooms');
+const divUsers = document.querySelector('.div_users');
+const divChat = document.querySelector('.div_chat');
 const domain = ('http://127.0.0.1:8000/api/');
-const user_id = window.userID;
+const userId = window.userID;
+let currentRoomId = "";
+
+//функция получения токена
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length +
+
+    1));break;}}}return cookieValue;
+}
+//записываем токен в переменную
+const csrftoken = getCookie('csrftoken');
+
 
 // формируем случайный номер для WS для возможности одновременного открытия на разных вкладках
 let sockNum =  Math.floor(Math.random() * 100 + 1);
@@ -14,55 +35,113 @@ socket.onclose = async (event) => {
 
 socket.onopen = () => load_userCard();
 function load_userCard() {
-    viewProfil(user_id);
+    viewProfil(userId);
+    viewRooms();
+    viewUsers();
 };
 
 socket.onmessage = function(event) {
     let data = JSON.parse(event.data);
     console.log(data);
 
-    if ('message' in data) {
-
+    if ('MessageList' in data) {
+        showChat(data);
     };
-
-
 };
 
 // Запрос данных пользователя
 function viewProfil(userId) {
-    console.log(userId);
     fetch(domain + 'user/' + userId +'/')
         .catch(err => console.log(err))
         .then(response => response.json())
-        .then(result => showProfile(result));
+        .then(result => showProfil(result));
+};
+
+function viewUsers() {
+    console.log('Пользователи могут быть показаны');
+    fetch(domain + 'user/')
+        .catch(err => console.log(err))
+        .then(response => response.json())
+        .then(result => showUsers(result));
+};
+
+// Запрос списка комнат
+function viewRooms() {
+    fetch(domain + 'rooms/')
+        .catch(err => console.log(err))
+        .then(response => response.json())
+        .then(result => showRooms(result));
 };
 
 // Вывод профиля в браузер
-function showProfile(item) {
+function showProfil(item) {
     console.log(item);
     divProfil.innerHTML = `
     <div class="div_profil">
         <img class="img_avatar" src="${item.avatar}">
         <br>
-        <strong>Сменить аватарку:</strong><br>
-        <input id="avatar-input" type="file" accept="image/*"><br>
-        <button onclick="changeAvatar(${item.user})">отправить</button>
-        <p>ID: ${item.user}</p>
-        <p>Имя: ${item.first_name} <button onclick="changeFirstName(${item.user})">изменить</button></p>
+        <strong>Сменить аватарку:</strong>
+        <br>
+        <input class="btn" id="avatar-input" type="file" accept="image/*"><br>
+        <button class="btn" onclick="changeAvatar(${item.user})">отправить</button>
+        <p style="text-align: left;">ID: ${item.user}</p>
+        <p style="text-align: left;">
+        Имя: ${item.first_name}
+        <button class="btn" onclick="changeFirstName(${item.user})">изменить</button>
+        </p>
         <h4 class="message" id="message"></h4>
     </div>
     `;
 };
 
+// Вывод Пользователей в браузер
+ function showUsers(item) {
+    let listUser = '';
+    for (let key in item) {
+        const newStringOnUser = `<tr><td><img class="img_avatar" width="22" height="22" src="${item[key].avatar}"><b><button style="width: 200px;" class="btn" onclick="jampInUser(${item[key].user})">${item[key].first_name}</button></b></td></tr>`;
+        listUser = listUser + newStringOnUser;
+    };
+    listUser = '<table>' + listUser + '</table><br>'
+    divUsers.innerHTML = listUser;
+};
+
+// Вывод комнат в браузер
+function showRooms(item) {
+//    delete item.RoomList;
+    let listRoom = '';
+    for (let key in item) {
+        const newStringOnRoom = `<tr><td><b><button style="width: 200px;" class="btn" onclick="jampInRoom(${item[key].id})">${item[key].name}</button></b></td>
+        <td><button class="btn" onclick="deleteRoom(${item[key].id})">🗑</button></td>
+        <td><button class="btn" onclick="changeRoom(${item[key].id})">🔄</button></td></tr>`;
+        listRoom = listRoom + newStringOnRoom;
+    };
+    listRoom = '<table>' + listRoom + '</table><br>'
+    listRoom = listRoom + `<input type="text" id="input_room" name="name_new_room" size="32" placeholder="Введите имя новой комнаты"><br>`
+    listRoom = listRoom + `<button style="width: 275px;" class="btn btn_new_room">Создать комнату</button>`
+    divRooms.innerHTML = listRoom;
+
+    // Создание новой комнаты
+    document.querySelector('.btn_new_room').addEventListener('click', () => {
+        let name = document.getElementById("input_room");
+        if (name.value !== "") {
+            socket.send(JSON.stringify({'create_room': name.value}));
+            console.log({'create_room': name.value});
+            name.value = "";
+            viewRooms();
+        };
+    });
+};
+
 // замена аватара
-async function changeAvatar(user_id) {
+async function changeAvatar(userId) {
     const formData = new FormData();
     let fileField = document.querySelector('#avatar-input');
     if (fileField.files[0]) {
         formData.append('avatar', fileField.files[0]);
         try {
-            const response = await fetch(domain + 'user/' + user_id +'/', {
+            const response = await fetch(domain + 'user/' + userId +'/', {
                 method: 'PATCH',
+                headers: {'X-CSRFToken': csrftoken},    //записываем токен в загаловок для прохождения безопасности
                 body: formData
             });
             const result = await response.json();
@@ -71,16 +150,80 @@ async function changeAvatar(user_id) {
             console.log('Все пропало!!!');
             console.error('Ошибка:', error);
         }
-        viewProfil(user_id);
+        viewProfil(userId);
+        viewUsers();;
     } else {
         document.querySelector('#message').innerText = 'выберите файл!';
     };
 };
 
 //замена имени
-function changeFirstName(user_id) {
+function changeFirstName(userId) {
     let name = prompt('Введите новое имя');
-    socket.send(JSON.stringify({'order': 'changeFirstName', 'id': user_id, 'name': name }));
-    console.log({'order': 'changeFirstName', 'id': user_id, 'name': name })
-    viewProfil(user_id);
+    socket.send(JSON.stringify({'replacement': 'changeFirstName', 'id': userId, 'name': name }));
+    console.log({'replacement': 'changeFirstName', 'id': userId, 'name': name })
+    viewProfil(userId);
+};
+// удаление комнаты
+function deleteRoom(item) {
+    socket.send(JSON.stringify({'delete_room': item}));
+    console.log(item, " комната удалена")
+    viewRooms();
+};
+// изменение названия
+function changeRoom(item) {
+    let name = prompt('Введите новое имя');
+    socket.send(JSON.stringify({'change_room': item, 'name': name }));
+    console.log(item)
+    viewRooms();
+};
+//переход вчат комнату
+function jampInRoom(item) {
+    console.log("перешел на комноту", item)
+    // Запрашиваем список сообщений комнаты в !!! командном канале
+    socket.send(JSON.stringify({'load': 'messageList', 'room_id': item}));
+    currentRoomId = item;
+    console.log({'load': 'messageList', 'room_id': item});
+    console.log(currentRoomId);
+     // Запрашиваем на сервере подключение к выбранной комнате и отключение от прежней (если была)
+//    chatSocket.send(JSON.stringify({'usersendcommandroom': 'roomselect', 'newroom_id': id, 'oldroom_id': currentRoom}));
+//    currentRoom = id; // после отключения обновляем id (для следующего переподключения)
+};
+// переход в чат с пользователем
+function jampInUser(item) {
+    console.log("перешел в час с пользователем ID ", item)
+};
+
+// выводим чат в браузер
+function showChat(item) {
+    divChat.innerHTML = `<h3 style="text-align: center;">Комната: ${item['MessageList']}</h3>
+    <textarea class="textarea" name="textarea"></textarea><br>
+    <input class="message_box" type="text" id="input_message" name="input_message" placeholder="Введите сообщение"><br>
+    <button class="btn btn_message">Отправить</button>`;
+    delete item.MessageList;
+    let textarea = document.querySelector('.textarea');
+   // currentRoomId = 9;
+    for (let messageElement in item) {
+        for (let key in item[messageElement]) {
+            let newString = `${key}: ${item[messageElement][key]}\n`;
+            textarea.value += newString;
+        };
+    };
+
+    // Отправка сообщения
+    document.querySelector('.btn_message').addEventListener('click', () => {
+        let message = document.getElementById("input_message");
+        if (message.value !== "") {
+            socket.send(JSON.stringify({'usersendcommandroom': 'message', 'room_id': currentRoomId, 'userid': userId, 'message': message.value}));
+            console.log({'usersendcommandroom': 'message', 'room_id': currentRoomId, 'user': userId, 'message': message.value});
+            message.value = "";
+        };
+    });
+//
+//    // слушаем сокет и принимаем входящие сообщения от подключенной комнаты
+//    chatSocket.onmessage = function(event) {
+//        let data = JSON.parse(event.data);
+//        console.log(data);
+//        textarea.value += `${data['name']}: ${data['message']}\n`;
+//    };
 };
